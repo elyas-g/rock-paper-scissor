c7f91c4eb06b840635a4106088e38c07030e1bca
elyas-g <elyasgetnet23@gmail.com>
2025-11-27 17:31:39 +0300

    Use class to the existing code

--- script.js
+++ script.js
@@ -1,97 +1,81 @@
-const body = document.querySelector("body");
-const displayContainer = document.createElement("div");
+class Game {
+  constructor() {
+    this.body = document.querySelector("body");
+    this.displayContainer = document.createElement("div");
+    this.body.appendChild(this.displayContainer);
 
-function getComputerChoice() {
-  const index = Math.round(2 * Math.random() + 1);
-  const litsOfChoice = ["rock", "paper", "scissors"];
-  return litsOfChoice[index - 1];
+    this.rockButton = document.createElement("button");
+    this.rockButton.innerText = "Rock";
+
+    this.paperButton = document.createElement("button");
+    this.paperButton.innerText = "Paper";
+
+    this.scissorsButton = document.createElement("button");
+    this.scissorsButton.innerText = "Scissors";
+
+    this.body.appendChild(this.rockButton);
+    this.body.appendChild(this.paperButton);
+    this.body.appendChild(this.scissorsButton);
+
+    this.rockButton.addEventListener("click", () => this.playRound("rock"));
+    this.paperButton.addEventListener("click", () => this.playRound("paper"));
+    this.scissorsButton.addEventListener("click", () =>
+      this.playRound("scissors")
+    );
+  }
+
+  getComputerChoice() {
+    const index = Math.floor(Math.random() * 3);
+    const litsOfChoice = ["rock", "paper", "scissors"];
+    return litsOfChoice[index];
+  }
+
+  playRound(humanChoice) {
+    const computerChoice = this.getComputerChoice();
+
+    const HumanChoice = document.createElement("p");
+    HumanChoice.innerText = `User: ${humanChoice}`;
+
+    const ComputerChoiceDisplay = document.createElement("p");
+    ComputerChoiceDisplay.innerText = `Bot: ${computerChoice}`;
+
+    const displayResult = document.createElement("p");
+
+    if (humanChoice === computerChoice) {
+      displayResult.innerText = "Tie!!!";
+    }
+
+    if (humanChoice === "rock") {
+      if (computerChoice === "paper") {
+        displayResult.innerText = "You lose! Paper beats Rock. Try again!";
+      }
+      if (computerChoice === "scissors") {
+        displayResult.innerText =
+          "You win! Rock beats Scissors. Congratulations!";
+      }
+    }
+
+    if (humanChoice === "paper") {
+      if (computerChoice === "scissors") {
+        displayResult.innerText = "You lose! Scissors beats Paper. Try again!";
+      }
+      if (computerChoice === "rock") {
+        displayResult.innerText = "You win! Paper beats Rock. Congratulations!";
+      }
+    }
+
+    if (humanChoice === "scissors") {
+      if (computerChoice === "rock") {
+        displayResult.innerText = "You lose! Rock beats Scissors. Try again!";
+      }
+      if (computerChoice === "paper") {
+        displayResult.innerText =
+          "You win! Scissors beats paper. Congratulations!";
+      }
+    }
+    this.displayContainer.appendChild(HumanChoice);
+    this.displayContainer.appendChild(ComputerChoiceDisplay);
+    this.displayContainer.appendChild(displayResult);
+  }
 }
-
-function playRound(humanChoice, computerChoice) {
-  const displayResult = document.createElement("p");
-
-  const HumanChoice = document.createElement("p");
-  HumanChoice.innerText = `User: ${humanChoice}`;
-
-  const ComputerChoice = document.createElement("p");
-  ComputerChoice.innerText = `Bot: ${computerChoice}`;
-
-  displayContainer.appendChild(HumanChoice);
-  displayContainer.appendChild(ComputerChoice);
-
-  if (humanChoice === computerChoice) {
-    displayResult.innerText = "Tie!!!";
-    displayContainer.appendChild(displayResult);
-    body.appendChild(displayContainer);
-    return "";
-  }
-
-  if (humanChoice === "rock") {
-    if (computerChoice === "paper") {
-      displayResult.innerText = "You lose! Paper beats Rock. Try again!";
-      displayContainer.appendChild(displayResult);
-      body.appendChild(displayContainer);
-      return "computer";
-    }
-    if (computerChoice === "scissors") {
-      displayResult.innerText =
-        "You win! Rock beats Scissors. Congratulations!";
-      displayContainer.appendChild(displayResult);
-      body.appendChild(displayContainer);
-      return "human";
-    }
-  }
-
-  if (humanChoice === "paper") {
-    if (computerChoice === "scissors") {
-      displayResult.innerText = "You lose! Scissors beats Paper. Try again!";
-      displayContainer.appendChild(displayResult);
-      body.appendChild(displayContainer);
-      return "computer";
-    }
-    if (computerChoice === "rock") {
-      displayResult.innerText = "You win! Paper beats Rock. Congratulations!";
-      displayContainer.appendChild(displayResult);
-      body.appendChild(displayContainer);
-      return "human";
-    }
-  }
-
-  if (humanChoice === "scissors") {
-    if (computerChoice === "rock") {
-      displayResult.innerText = "You lose! Rock beats Scissors. Try again!";
-      displayContainer.appendChild(displayResult);
-      body.appendChild(displayContainer);
-      return "computer";
-    }
-    if (computerChoice === "paper") {
-      displayResult.innerText =
-        "You win! Scissors beats paper. Congratulations!";
-      displayContainer.appendChild(displayResult);
-      body.appendChild(displayContainer);
-      return "human";
-    }
-  }
-}
-
-//buttons
-const rockButton = document.createElement("button");
-rockButton.innerText = "Rock";
-const paperButton = document.createElement("button");
-paperButton.innerText = "Paper";
-const scissorsButton = document.createElement("button");
-scissorsButton.innerText = "Scissors";
-
-body.appendChild(rockButton);
-body.appendChild(paperButton);
-body.appendChild(scissorsButton);
-
-rockButton.addEventListener("click", () => {
-  playRound("rock", getComputerChoice());
-});
-paperButton.addEventListener("click", () => {
-  playRound("paper", getComputerChoice());
-});
-scissorsButton.addEventListener("click", () => {
-  playRound("scissors", getComputerChoice());
-});
+const game1 = new Game();
